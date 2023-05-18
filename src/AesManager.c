96d60a5dc3f0358726569b64fdbd64c73d316d0f
BengtCruz <bengtsvedling@hotmail.com>
2023-05-19 00:37:09 +0200

    Improvement &
    start of application development

---
+++ src/AesManager.c
@@ -0,0 +1,16 @@
+/**
+ * @file AesManager.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-05-19
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <aes.h>
+
+#define AES_BLOCK_SIZE 16
